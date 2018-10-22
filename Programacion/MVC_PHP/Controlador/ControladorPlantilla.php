cbe29d7a8da1a705187ba077ee99ce0e85f85126
EdgarSando <edsan_6@hotmail.com>
2018-10-21 22:13:49 -0500

    se convirtió en php los módulos en la carpeta programción

---
+++ Programacion/MVC_PHP/Controlador/ControladorPlantilla.php
@@ -0,0 +1,8 @@
+<?php 
+		class ControllerPlantilla{
+
+			public function plantilla(){
+							
+				include "Vista/plantilla.php";
+			}
+		}
---
+++ Programacion/MVC_PHP/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+	require_once "Controlador/ControladorPlantilla.php";
+
+	$plantilla = new ControllerPlantilla();
+	$plantilla -> plantilla();
